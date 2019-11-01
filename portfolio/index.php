a5ec2b6130c276dba03c6887f4776ffa24a24d7c
jeremy <mainard17@outlook.fr>
2019-11-01 16:26:31 +0100

    portfolio

---
+++ portfolio/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<title> Portfolio </title>
+		<link href="index.css" rel="stylesheet" media="all" type="text/css">
+		</head>
+	<body>
+		<div id="center">
+		<p id="textP"> Page permettant de montrer mes <a href="https://github.com/JMainard">connaissances </a>  </p>
+		</div>
+
+
+		<div id="zone_bouton" 
+			onmouseover="document.getElementById('le_bouton').style.visibility='visible';"
+			onmouseout="document.getElementById('le_bouton').style.visibility='hidden';">
+			<a href="pagePresentation.html">
+			<button id="le_bouton" >Cliquez moi !</button>
+			</a>
+		</div>
+		
+
+	</body>
+</html> 
+		
